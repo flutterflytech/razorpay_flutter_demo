72ec719b9d799d6000ff1169d16496c5a044bb81
mobcoder-saurav <sauravbansal448@gmail.com>
2021-03-06 11:47:07 +0530

    razorpay_payment_gateway_example

---
+++ android/app/src/main/kotlin/com/example/rajorpay_flutter_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rajorpay_flutter_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homePage.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:rajorpay_flutter_demo/razorpay/check.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Payment Integration"),
+      ),
+      body: Center(
+        child: Container(
+          child: RaisedButton(
+            color: Colors.green,
+            onPressed: () => Navigator.of(context).pushAndRemoveUntil(
+              MaterialPageRoute(
+                builder: (context) => CheckRazor(),
+              ),
+                  (Route<dynamic> route) => false,
+            ),
+            child: Text(
+              "Pay Now",
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
